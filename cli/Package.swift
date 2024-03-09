fcd02eb91eb318477d7864d0422ed8a672f89da7
tanaka <lemo.nade.engineer.room@gmail.com>
2024-03-09 23:40:01 +0900

    ターゲット名を変更

--- cli/Package.swift
+++ cli/Package.swift
@@ -5,10 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "home-observer",
+    platforms: [.macOS(.v14)],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "home-observer")
+        .executableTarget(name: "HomeObserver"),
+        .testTarget(name: "HomeObserverTests", dependencies: ["HomeObserver"]),
     ]
 )
