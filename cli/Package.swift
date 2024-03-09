59b1da217352fe11fb99daed05f405c2cf263266
tanaka <lemo.nade.engineer.room@gmail.com>
2024-03-09 23:45:34 +0900

    Console系を使用するためVaporの依存を追加

--- cli/Package.swift
+++ cli/Package.swift
@@ -6,6 +6,9 @@ import PackageDescription
 let package = Package(
     name: "home-observer",
     platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.92.4"),
+    ],
     targets: [
         .executableTarget(name: "HomeObserver"),
         .testTarget(name: "HomeObserverTests", dependencies: ["HomeObserver"]),
